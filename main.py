9a26f9e1984d32de423cb9800cffc2ae173a6f58
Jiří Majer <jiri.majer@outlook.cz>
2026-05-09 23:13:04 +0200

    Add programmer information to main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 PRESENTER = "Jiří Majer"
+PROGRAMMER = "Not yet"
 
 
 def main():
     print("Welcome to GitHub demo.")
     print(f"Presented by: {PRESENTER}")
+    print(f"Programmed by: {PROGRAMMER}")
 
 
 if __name__ == "__main__":
